442c30e8d8c906539b88868a2bae21cbe6e1a2e8
Shafayet Zim <43902599+ShafayetZim@users.noreply.github.com>
2021-12-15 15:53:24 +0600

    Add files via upload

---
+++ prog4-(string format).py
@@ -0,0 +1,9 @@
+# string format
+num1 = 20
+num2 = 40
+
+print(f"{num1} + {num2} = {num1+num2}")
+
+# remove new line
+print("Zim loves", end=" ")  # end skips newline and "space" add between them
+print("Burger")
