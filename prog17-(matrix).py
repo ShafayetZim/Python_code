bc44964bbaaf2d85095466d5c63a98b3ef6f0cf7
Shafayet Zim <43902599+ShafayetZim@users.noreply.github.com>
2021-12-31 10:08:40 +0600

    Add files via upload

---
+++ prog17-(matrix).py
@@ -0,0 +1,10 @@
+# print every element in matrix
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6]
+]
+
+for row in matrix:  # every row comes in
+    for col in row:  # that row placed into column.
+        print(col)
