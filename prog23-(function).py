c9171a7442066df0628fbcf93dff0e0fcdf3c83a
Shafayet Zim <43902599+ShafayetZim@users.noreply.github.com>
2022-01-05 10:02:05 +0600

    Add files via upload

---
+++ prog23-(function).py
@@ -0,0 +1,16 @@
+# function
+
+def add(x, y, z):
+    sum = x + y + z
+    print(sum)
+
+def sub(x, y):
+    res = x - y
+    print(res)
+
+def message():
+    print("no parameter")
+
+add(2, 3, 5)
+sub(10, 3)
+message()
