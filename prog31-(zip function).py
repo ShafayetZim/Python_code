96c8345a8319918a73364ea15f4058a1c408dffc
Shafayet Zim <43902599+ShafayetZim@users.noreply.github.com>
2022-01-12 12:50:55 +0600

    Add files via upload

---
+++ prog31-(zip function).py
@@ -0,0 +1,10 @@
+# zip function
+
+roll = [101, 102, 103, 104]
+name = ["sakib", "tamim", "liton", "musfiq"]
+
+result = list(zip(roll, name))
+ans = list(zip(roll, name, "ABCD"))
+
+print(result)
+print(ans)
