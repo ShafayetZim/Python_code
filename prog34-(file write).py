360680d1c04e1355b878f620f9e5f3117aee9f38
Shafayet Zim <43902599+ShafayetZim@users.noreply.github.com>
2022-01-14 10:49:47 +0600

    file writing added

---
+++ prog34-(file write).py
@@ -0,0 +1,9 @@
+# file write
+
+# w means write- it overrides the existing code
+# a means append- allows adding new lines with existing
+file = open("text.txt", "a")
+
+file.write("\nSajid - American International University")
+
+file.close()
